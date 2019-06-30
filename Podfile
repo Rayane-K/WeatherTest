584fd182a5592346f33c46fc4f991449e70876f8
Kurrimboccus Rayane <RKurrimboccus@oui.sncf>
2019-06-30 19:49:19 +0200

    setup cocoapods

---
+++ Podfile
@@ -0,0 +1,21 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'WeatherTest' do
+  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+  pod 'SwiftDate', '5.1'
+
+  # Pods for WeatherTest
+
+  target 'WeatherTestTests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+  target 'WeatherTestUITests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+end
---
+++ WeatherTest/WeatherAPI.swift
@@ -0,0 +1,13 @@
+//
+//  WeatherAPI.swift
+//  WeatherTest
+//
+//  Created by Kurrimboccus Rayane on 30/06/2019.
+//  Copyright © 2019 Kurrimboccus Rayane. All rights reserved.
+//
+
+import Foundation
+
+struct WeatherAPI {
+    static let apiUrl = "http://www.infoclimat.fr/public-api/gfs/json?_ll=48.85341,2.3488&_auth=Bx1QRwV7ASMCL1dgAXcELQVtADVeKFdwC3cDYABlUSwGbQJjBmZXMVA%2BWyYGKQQyVHlSMQw3VGRQO1IqXC5TMgdtUDwFbgFmAm1XMgEuBC8FKwBhXn5XcAtpA2MAa1EsBmQCbgZgVytQPVs8BjQELlRmUjQMMlRzUCxSNFw0UzYHZVA9BWcBZQJuVzABMAQvBSkAZV41V24LYAMyAG5RYAYzAjcGZldhUG1bPQY2BC5Ub1IwDDNUZFA6UjNcOFM3B3tQKwUfARACcFd1AXMEZQVwAH1eNFcxCzw%3D&_c=d301ddf413fe089b63fec35070a18b9b"
+}
