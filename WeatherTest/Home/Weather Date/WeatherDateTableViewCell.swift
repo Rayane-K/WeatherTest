a8abc03f9aaaacbe1f1da3a2fcf605a7eb1789ac
Kurrimboccus Rayane <RKurrimboccus@oui.sncf>
2019-06-30 20:10:51 +0200

    add WeatherDateTableViewCell

---
+++ WeatherTest/Home/Weather Date/WeatherDateTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  WeatherDateTableViewCell.swift
+//  WeatherTest
+//
+//  Created by Kurrimboccus Rayane on 30/06/2019.
+//  Copyright © 2019 Kurrimboccus Rayane. All rights reserved.
+//
+
+import UIKit
+
+class WeatherDateTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
---
+++ WeatherTest/Home/Weather Date/WeatherDateViewModel.swift
@@ -0,0 +1,9 @@
+//
+//  WeatherDateViewModel.swift
+//  WeatherTest
+//
+//  Created by Kurrimboccus Rayane on 30/06/2019.
+//  Copyright © 2019 Kurrimboccus Rayane. All rights reserved.
+//
+
+import Foundation
